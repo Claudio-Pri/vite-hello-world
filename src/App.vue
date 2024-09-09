c42500b983620714a849cb96e9c45ae9d8538da4
Claudio-Pri <priv.claudio@gmail.com>
2024-09-09 16:33:38 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<script>
+/*
+Passaggi import componente:
+1) Importazione componente
+2) Dichiarazione del componente
+3) Utilizzo del componente
+*/
+
+// 1) Importazione componente
+import AppHeader from './components/AppHeader.vue';
+
+  export default {
+  data() {
+    return { 
+      count: 0
+     }
+  },
+  //2) Dichiarazione del componente (prima o dopo data())
+  components: {
+    AppHeader
+  },
+  methods: {
+    incrementCount() {
+      this.count++;
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- N.B. Template vuole 1 solo figlio -->
+  <div>
+    <!-- 3) Utilizzo del componente come tag htm (self-closing) -->
+    <AppHeader/>
+    <button @click="incrementCount()">
+      {{ count }}
+    </button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,27 @@
+<script>
+  export default {
+  data() {
+    return { 
+      message: 'template vite + vue'
+     }
+  },
+}
+</script>
+
+<template>
+  <header>
+    <h1>
+      {{ message }}
+    </h1>
+  </header>
+</template>
+
+<style scoped>
+header {
+  background-color: aqua;
+  text-align: center;
+}
+h1 {
+  color: lawngreen;
+}
+</style>
